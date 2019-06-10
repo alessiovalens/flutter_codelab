4d78e3ebbcb32b91ef23f13f590779b551a27862
alessiovalens <alessiovalens@gmail.com>
2019-06-10 22:21:53 +0200

    Initial commit

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -1,11 +1,6 @@
-import 'package:codelab/model/product.dart';
 import 'package:flutter/material.dart';
-import '../model/productsRepository.dart';
 
 class HomePage extends StatelessWidget {
-
-  final List<Product> products = ProductsRepository.loadProducts(Category.all);
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
